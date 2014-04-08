d97bc7ffcf41710d9296c03eac6151d45303548e
Dmitry Sukhikh <china@pozitive.biz>
2014-04-08 11:19:14 +0700

    Cleaned themplates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,3 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-
-  layout :layout_for_page
-
-  private
-
-  def layout_for_page
-    case params[:id]
-    when 'home'
-      'home'
-    when 'theme_panorama'
-      'panorama'
-    else
-      'application'
-    end
-  end
 end
