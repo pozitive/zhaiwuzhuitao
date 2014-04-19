40af85dc36d82094303fb024ed7a5890c555a3da
Dmitry Sukhikh <china@pozitive.biz>
2014-04-19 16:13:25 +0700

    Unicrorn and nginx config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,31 @@
+require 'capistrano/rbenv'
+require 'capistrano/bundler'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
 # config valid only for Capistrano 3.1
 lock '3.2.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+server '109.120.166.48', roles: [:web, :app, :db, :workers], ssh_options: {
+  user: "deploy",
+  forward_agent: true
+}
+
+set :application, 'zhaiwuzhuitao'
+set :repo_url, 'git@github.com/dolg-vozvrat/zhaiwuzhuitao'
+
+set :default_environment, { 'PATH' => '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH' }
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.1.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, "/home/deploy/apps/zhaiwuzhuitao"
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -34,6 +51,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
+
 namespace :deploy do
 
   desc 'Restart application'
@@ -41,6 +62,21 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  task :setup_config do
+    on roles(:app) do
+      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/zhaiwuzhuitao"
+      sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_zhaiwuzhuitao"
+      execute "mkdir -p #{current_path}/tmp/pids"
+      execute "mkdir -p #{shared_path}/config"
+    end
+  end
+
+  task :symlink_config do
+    on roles(:app) do
+      execute "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     end
   end
 
