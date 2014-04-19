136322aab3a440ed587e30235a83d1fab18f3d4a
Dmitry Sukhikh <china@pozitive.biz>
2014-04-19 17:00:05 +0700

    Fixed duplictate name unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.soyuz.sock"
+listen "/tmp/unicorn.zhaiwuzhuitao.sock"
 worker_processes 2
 timeout 30
