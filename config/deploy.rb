7a7f59ca42057895d694f61f338d68c870cc972e
Dmitry Sukhikh <china@pozitive.biz>
2014-09-28 21:42:22 +0400

    Change deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,12 +56,18 @@ set(:executable_config_files, %w(
 ))
 
 namespace :deploy do
+  %w[start stop].each do |command|
+    desc "#{command} unicorn server"
+    task command  do
+      on roles(:app) do
+        execute "/etc/init.d/unicorn_zhaiwuzhuitao #{command}"
+      end
+    end
+  end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+  task :graceful_stop do
+    on roles(:app) do
+      execute "lsof /tmp/unicorn.zhaiwuzhuitao.sock | sed -n '2p' | awk '{print $2}' | xargs kill -QUIT"
     end
   end
 
@@ -84,11 +90,8 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      execute "lsof /tmp/unicorn.zhaiwuzhuitao.sock | sed -n '2p' | awk '{print $2}' | xargs kill -QUIT"
+      execute "/etc/init.d/unicorn_zhaiwuzhuitao start"
     end
   end
-
 end
